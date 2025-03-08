afbf0d8c2a5940ccde29f15371206c70b9c14f79
NE7K <sjjang16@naver.com>
2025-03-08 15:52:29 +0900

    Create File

--- WebCrawler/Naver/BlogScrollCrawler.py
+++ WebCrawler/Naver/BlogScrollCrawler.py
@@ -29,7 +29,6 @@ from bs4 import BeautifulSoup
 #     print(list[i]['href'] + '\n')
 #     print(list[i].text + '\n')
 
-
 def searchData(search):
     url = f'https://s.search.naver.com/p/review/49/search.naver?ssc=tab.blog.all&api_type=8&query={search}&start=1&nx_search_query=&nx_and_query=&nx_sub_query=&ac=0&aq=0&spq=0&sm=tab_hty.top&nso=&prank=30&ngn_country=KR&lgl_rcode=02192107&fgn_region=&fgn_city=&lgl_lat=37.48924&lgl_long=126.816765&enlu_query=IggCAF6CULgpAAAA94CCHN2nXekVm8x1ohP7Uf%2BKYUA4L3vA3r8bxtIuZ6FvuGnRvNov%2BA3yj4REvrcijXNSSXl8BmfouYfEaXfsTZNApaEaUzaurcMvLmmqaVa%2BQiqNxuQJ2SvR1xNxQ%2B%2BFxxoHf6wfUSHv1eqcy1inB%2FNeP5Dl%2FZm6pKZXF%2FZcmzw%3D&enqx_theme=IggCAEeCULgRAAAAh%2FDtntZaiMLGh3DOFtIyqw%2Ft3q4clEos1p2O1NTRYn4NUJzpY2XWim4radqdYrSZDNiELd6nYNJwytlWzreKNul2xLfylQ7CjZHVqCLhCfDfLmMEOVaPYbdbw2RxiX5kzvQc%2BH53BTcRGA4hvzLyCw%3D%3D&abt='
     result = requests.get(url)
