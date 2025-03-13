f7292ec6173742af318d39dd616028e76afcb253
NE7K <sjjang16@naver.com>
2025-03-13 20:27:58 +0900

    Added Auto Login System

--- WebCrawler/Instagram/instagram.py
+++ WebCrawler/Instagram/instagram.py
@@ -19,7 +19,7 @@ driver = webdriver.Chrome()
 driver.get('https://www.instagram.com')
 
 # 코드 정지
-time.sleep(5)
+time.sleep(3)
 
 id = driver.find_elements(By.CSS_SELECTOR, 'input[name="username"]')
 id[0].send_keys(os.getenv('id'))
@@ -27,9 +27,11 @@ id[0].send_keys(os.getenv('id'))
 passwd = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 passwd.send_keys(os.getenv('pw'))
 
-# pressClick = driver.find_element(By.CSS_SELECTOR, '._acan button').text
-# print(pressClick)
+# pressClick = driver.find_element(By.CSS_SELECTOR, '._acan button').click()
+pressClick = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').send_keys(Keys.ENTER)
 
+# 버튼 상위 div > x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1
+# 버튼 class  _acan _acap _acas _aj1- _ap30
 
 # x1anpbxc x1h5jrl4 xyorhqc xmn8rco => div class에 있는 span 찾기
 # e = driver.find_element(By.CSS_SELECTOR, '.x1anpbxc span').text
@@ -39,5 +41,7 @@ passwd.send_keys(os.getenv('pw'))
 # print(getInput.text)
 
 # 종료 방지
-input('enter')
+# input('enter')
+
+time.sleep(1000000000)
 # driver.close()
