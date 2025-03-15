e04c68718a47c4a9e09b64009703211ef46d7af1
NE7K <sjjang16@naver.com>
2025-03-15 16:23:33 +0900

    Create File

---
+++ WebCrawler/Naver/logincapcha.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://nid.naver.com/nidlogin.login?svctype=262144')
+
