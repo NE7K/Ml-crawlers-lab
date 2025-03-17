7ebd26ada389f2b0a27673c51941b398594c23b2
NE7K <sjjang16@naver.com>
2025-03-18 00:52:35 +0900

    Test

--- WebCrawler/test2.py
+++ WebCrawler/test2.py
@@ -1,1 +1,19 @@
+# note rename
 import os
+# note copy file
+import shutil
+
+FileList = os.listdir('testFile')
+
+# for i in os.listdir('testFile'):
+#     os.rename(f'testFile/{i}', f'testFile/a{i}')
+
+# note 절대경로 = r
+for i in os.listdir('testFile'):
+    shutil.copy(f'testFile/{i}', f'testFile2/{i}')
+    
+# 경로 합치기
+os.path.join()
+
+# 지금 경로
+os.getcwd()
