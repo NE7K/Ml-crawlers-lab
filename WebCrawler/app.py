db416fe3332df2c0a60f6a265690d0190dee6645
NE7K <sjjang16@naver.com>
2025-03-20 00:35:57 +0900

    Fixed File

--- WebCrawler/app.py
+++ WebCrawler/app.py
@@ -9,9 +9,13 @@
 
 # 오브젝트 한줄컷 기계
 class Hero:
-    def __init__(self):
-        self.q = 'eat'
+    def __init__(self, 구멍):
+        self.q = 구멍
         self.w = 'snowball'
+    
+    def hello(self):
+        print('hello')
         
-omg = Hero()
+omg = Hero('구멍띠')
 print(omg.q)
+omg.hello()
