a94d8a462164fbd4f523e2c4edc1b2b373d2a28e
NE7K <sjjang16@naver.com>
2025-03-19 23:26:38 +0900

    Subject

---
+++ WebCrawler/app.py
@@ -0,0 +1,17 @@
+# nunu = {
+#   'q' : 'eat',
+#   'w' : 'snowball'
+# }
+# garen = {
+#   'q' : 'strike',
+#   'w' : 'courage'
+# }
+
+# 오브젝트 한줄컷 기계
+class Hero:
+    def __init__(self):
+        self.q = 'eat'
+        self.w = 'snowball'
+        
+omg = Hero()
+print(omg.q)
