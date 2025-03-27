6cc8220d6c57b7ca326e016befeeea67e6e99ae7
NE7K <sjjang16@naver.com>
2025-03-27 14:45:49 +0900

    Resizing image basic

--- WebCrawler/Others/ImageResizing.py
+++ WebCrawler/Others/ImageResizing.py
@@ -1,3 +1,8 @@
+#note image resize import - how to use ? > pip install Pillow
 from PIL import Image
 
-Image.open()
+img = Image.open('Images/photo1.jpg')
+#info 이미지 리사이즈해서 담아두면 화질 깨짐
+img = img.resize((300,500))
+
+img.save('Images/new_photo1.jpg')
