f27556277666f99f10dfb7a161d06667f11fc333
NE7K <sjjang16@naver.com>
2025-03-31 02:29:34 +0900

    insert html code mail

--- WebCrawler/Others/SendEmail.py
+++ WebCrawler/Others/SendEmail.py
@@ -6,11 +6,24 @@ from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
 
+# info html import
+from email.mime.multipart import MIMEMultipart
+
 # .env file load
 load_dotenv()
 
-text = os.getenv('context')
-msg = MIMEText(text) 
+# info html 기반 email 보내려고 할 때
+msg = MIMEMultipart('alternative')
+내용 = """
+<h4> 테스트임 ㄷㄷ </h4>
+<button> 버튼 띠 </button>
+"""
+part1 = MIMEText(내용, "html")
+msg.attach(part1)
+
+# info 그냥 텍스트만 보내려고 할 때
+# text = os.getenv('context')
+# msg = MIMEText(text) 
  
 msg['Subject'] = os.getenv('title')
 msg['From'] = os.getenv('email')
