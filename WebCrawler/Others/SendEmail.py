1f5ae2cff8442a72d2d8b46c07cfe127fd94d740
NE7K <sjjang16@naver.com>
2025-03-31 01:40:07 +0900

    reset system

--- WebCrawler/Others/SendEmail.py
+++ WebCrawler/Others/SendEmail.py
@@ -1,25 +1,24 @@
-#info smtp 방식 사용
-
+# note smtp 사용
 import smtplib
 from email.mime.text import MIMEText
-
+ 
 # .env 사용하려고 할 때
 from dotenv import load_dotenv
 import os
 
 # .env file load
 load_dotenv()
- 
-text = "SMTP Mail Auto Send Test"
+
+text = os.getenv('context')
 msg = MIMEText(text) 
  
-msg['Subject'] ="zz"
-msg['From'] = os.getenv('id')
-msg['To'] = os.getenv('poset')
+msg['Subject'] = os.getenv('title')
+msg['From'] = os.getenv('email')
+msg['To'] = os.getenv('post')
 print(msg.as_string())
  
 s = smtplib.SMTP( 'smtp.naver.com' , 587 ) 
 s.starttls() #TLS 보안 처리
-s.login( os.getenv('id') , os.getenv('pw') ) #네이버로그인
-s.sendmail( os.getenv('email'), os.getenv('post'), msg.as_string() )
+s.login( os.getenv('id') , os.getenv('pw') ) # 네이버로그인
+s.sendmail( '발송자이메일', '수신자이메일', msg.as_string() )
 s.close()
