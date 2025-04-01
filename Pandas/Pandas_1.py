19c5c47bef81268202e48a29ad3a44d40595df3c
NE7K <sjjang16@naver.com>
2025-04-01 13:31:17 +0900

    Fixed code

--- Pandas/Pandas_1.py
+++ Pandas/Pandas_1.py
@@ -4,13 +4,14 @@ test = pd.read_csv('credit.csv')
 
 # note pandas 라이브러리를 사용해서 자료를 뽑을 때 딕셔너리 뽑듯이 사용할 수 있음
 # info age data average
-print(test['나이'].mean())
+# print(test['나이'].mean())
 
-# info age data 최빈값
-print(test['나이'].mode())
+# # info age data 최빈값
+# print(test['나이'].mode())
 
-# info age data max and min result
-print(test['나이'].max())
-print(test['나이'].min())
+# # info age data max and min result
+# print(test['나이'].max())
+# print(test['나이'].min())
 
+# info data 분석 간단 명령어
 print(test['나이'].describe())
