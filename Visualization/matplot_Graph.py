a7eeac2ddc73075c57f6782faa0107354c86270b
NE7K <sjjang16@naver.com>
2025-05-07 01:53:04 +0900

    Added plt chart detail

--- Visualization/matplot_Graph.py
+++ Visualization/matplot_Graph.py
@@ -3,6 +3,24 @@ import pandas as pd
 # 시계열 데이터 사용시 필요 import
 import matplotlib.pyplot as plt
 
+# finance data download
+import yfinance as yf
+
+test = yf.download('^GSPC', start='1999-01-01', end='2025-01-01')
+
+# print(test)
+
 # [x], [y] 축 데이터
-plt.plot([1,2,3], [4,5,6])
+plt.plot(test.index, test['Close'], color='crimson')
+
+# x, y축 name 지정
+plt.xlabel('time')
+plt.ylabel('price($)')
+
+# 범례 출력
+plt.legend(['S&P 500'])
+
+# title
+plt.title('chart')
+
 plt.show()
