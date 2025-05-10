3fdc06b0ff1c4edf1bf7ab172fedc8738bc40ce2
NE7K <sjjang16@naver.com>
2025-05-11 01:50:36 +0900

    Feat : complete

--- Visualization/regression_analysis_3.py
+++ Visualization/regression_analysis_3.py
@@ -2,6 +2,13 @@ import numpy as np
 import statsmodels.api as sm
 import pandas as pd
 
-test = pd.read_csv('california_housing.csv')
+data = pd.read_csv('california_housing.csv')
 
-print(test)
+# print(data)
+
+# year, rooms, bedrooms : x, price : y
+model = sm.OLS(data['price'], data[['year', 'rooms', 'bedrooms']]).fit()
+print(model.summary())
+
+pr = model.predict([20, 1000, 300])
+print(pr)
