a874d64bdf53c6a0b742acee1af61aeb85080ee0
NE7K <sjjang16@naver.com>
2025-05-12 02:43:23 +0900

    Feat : data type change code and comments

--- Langchain/Langchain_1.py
+++ Langchain/Langchain_1.py
@@ -3,6 +3,15 @@ from langchain_openai import ChatOpenAI
 # info 가변적인 데이터를 넣고 답변을 받고 싶으면...
 from langchain_core.prompts import ChatPromptTemplate
 
+# result data를 parse하려면?
+from langchain_core.output_parsers import StrOutputParser
+
+# result data를 json으로 하려면?
+from langchain_core.output_parsers import SimpleJsonOutputParser 
+
+# pandas data frame으로는
+from langchain_core.output_parsers import PandasDataFrameOutputParser
+
 # info api key local 사용
 # import os
 # os.environ["OPENAI_API_KEY"] = '님들의 OpenAI API키'
@@ -19,12 +28,19 @@ model = ChatOpenAI(model='gpt-4o-mini')
 
 data = ChatPromptTemplate.from_messages(
     [
-        ('system', '너는 이제 영어를 한국어로 번역하는 봇이야'),
+        ('system', '너는 이제 영어를 한국어로 번역하는 봇이야. 딕셔너리 형태로 대답하는데 key는 "context"를 사용해서 대답해'),
         ('human', '{here}')    
         # ('ai', '여기는 예상 답변')
     ]
 )
 
-result = model.invoke(data.format_messages(here='i like kimchi'))
+# result = model.invoke(data.format_messages(here='i like kimchi'))
+# info 다른 방법으로는 LCEL 문법, data를 model에 넣음
+# info 이 문법이 매우 중요한 이유는 나중에 데이터를 끌고오면 데이터 | data | model 이런식으로 사용도 가능하기 때문
+# model에서 나온 데이터를 정제하고 싶으면 model | analysisdata이런식으로도 가능
+
+# FIX StrOutputParser() : parse, 
+result = (data | model | SimpleJsonOutputParser()).invoke({ 'here' : 'i like gray cat bro'})
+
 # result.content로 다른 내용은 제거
-print(result.content)
+print(result)
