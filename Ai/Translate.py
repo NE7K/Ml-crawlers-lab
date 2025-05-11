0c54bed58a49ebfc1f5df325c742f7d2de581dee
NE7K <sjjang16@naver.com>
2025-05-11 15:16:47 +0900

    Feat : complete get api

--- Ai/Translate.py
+++ Ai/Translate.py
@@ -1,1 +1,16 @@
+import deepl
 
+import os
+
+# 자동으로 env 파일 불러오는 import
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('key')
+
+auth_key = api_key  # Replace with your key
+translator = deepl.Translator(auth_key)
+
+result = translator.translate_text("Hello, world!", target_lang="KO")
+print(result.text)  # "Bonjour, le monde !"
